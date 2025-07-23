662641e1e556ff4dd98156082c5ef19929e6a034
unknown <noranakl2004@yahoo.com>
2025-07-23 19:22:18 +0300

    first commit

---
+++ Timer/Debug/Timer_Program.d
@@ -0,0 +1,15 @@
+Timer_Program.o Timer_Program.o: ../Timer_Program.c ../STD_Types.h \
+  ../MATH_BIT.h ../Timer_Interface.h ../Timer_Private.h \
+  ../Timer_Configuration.h ../Timer_Register.h
+
+../STD_Types.h:
+
+../MATH_BIT.h:
+
+../Timer_Interface.h:
+
+../Timer_Private.h:
+
+../Timer_Configuration.h:
+
+../Timer_Register.h:
